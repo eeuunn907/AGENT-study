f8688e8c8296a728a78906dbe47ebd313a064884
eeuunn <haspring@dsm.hs.kr>
2025-10-28 11:06:34 +0900

    Create ParseJson.kt

---
+++ src/main/kotlin/week1/domain/util/ParseJson.kt
@@ -0,0 +1,24 @@
+package week1.domain.util
+
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+
+
+fun JsonObject.parseJsonToMap(): Map<String, String> {
+    return this.mapValues { entry ->
+        when (val element = entry.value) {
+            is JsonPrimitive -> element.content
+            is JsonArray -> {
+                if (element.isNotEmpty()) {
+                    element.filterIsInstance<JsonPrimitive>().joinToString(separator = ", ") { it.content }
+                } else {
+                    throw IllegalArgumentException("Expected a JsonPrimitive in array for ${entry.key}")
+                }
+            }
+
+            else -> throw IllegalArgumentException("Expected a JsonPrimitive for ${entry.key}")
+        }
+
+    }
+}
