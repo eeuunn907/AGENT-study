bd60b43913a7662a16f101853e05c8cb78ed3c45
eeuunn <haspring@dsm.hs.kr>
2025-11-03 18:40:33 +0900

    Update OpenAI.kt

--- src/main/kotlin/week1-refactoring/domain/agent/OpenAI.kt
+++ src/main/kotlin/week1-refactoring/domain/agent/OpenAI.kt
@@ -5,11 +5,15 @@ import com.aallam.openai.api.chat.ChatMessage
 import com.aallam.openai.api.chat.ChatRole
 import com.aallam.openai.api.chat.ToolCall
 import com.aallam.openai.client.OpenAI
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonObject
-import `week1-refactoring`.domain.llm.model.LLMRequest
-import `week1-refactoring`.domain.llm.model.LLMResponse
+import `week1-refactoring`.domain.llm.dto.model.LLMRequest
+import `week1-refactoring`.domain.llm.dto.model.LLMResponse
 import `week1-refactoring`.domain.presentation.LLMRepository
 import `week1-refactoring`.domain.search.model.params
 import `week1-refactoring`.domain.search.model.provideFunction
@@ -21,30 +25,27 @@ class OpenAI : LLMRepository {
 
         val openAI = OpenAI(
             token = openAiKey
-        )
+        ) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                })
+            }
+        }
 
-        val chatMessages = mutableListOf(
-            ChatMessage(
-                role = ChatRole.System,
-                content = "안녕! 질문이 있어"
-            ),
-            ChatMessage(
-                role = ChatRole.User,
-                content = request.question
-            )
+        val chatMessages = listOf(
+            ChatMessage(ChatRole.System, "안녕! 질문이 있어"),
+            ChatMessage(ChatRole.User, request.question)
         )
 
         val response = openAI.chatCompletion(
             request = ChatCompletionRequest(
                 model = request.model,
                 messages = chatMessages,
-                functions = params,
+                functions = params
             )
         )
-
-
-        println(response.choices)
-
 
         val message = response.choices.first().message
         val finishReason = response.choices.first().finishReason
@@ -56,9 +57,6 @@ class OpenAI : LLMRepository {
 
             val functionName = functionCall.function.name
             val argumentsJson = Json.parseToJsonElement(functionCall.function.arguments).jsonObject
-
-            println(argumentsJson.toString())
-
             val argsMap = argumentsJson.toStringMap()
 
             val answer = provideFunction(functionName, argsMap)
