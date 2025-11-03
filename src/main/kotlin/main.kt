ffe7e1ffd96228636f65e571ea298dc1de691472
eeuunn <haspring@dsm.hs.kr>
2025-11-03 11:55:58 +0900

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,23 +11,23 @@ import week2.core.DeveloperAgent
 //    }
 //}
 
-fun main() {
-    println(
-        """
-        ============================
-          🤖 Developer Agent Started
-        ============================
-
-        📜 예시 요청:
-          1. Please develop and serve a webpage that displays hello world.
-          2. Please develop and serve a webpage that allows me to move a box with my mouse.
-          3. Develop and serve a webpage that shows the total number of page views.
-          4. Please develop and serve a simple todo webpage. The user should be able to add and delete todo and see all todos. Develop in a neumorphism style.
-          
-        💬 Enter your request:
-        """.trimIndent()
-    )
-
-    val request = readln()
-    DeveloperAgent.handleRequest(request)
-}
+//fun main() {
+//    println(
+//        """
+//        ============================
+//          🤖 Developer Agent Started
+//        ============================
+//
+//        📜 예시 요청:
+//          1. Please develop and serve a webpage that displays hello world.
+//          2. Please develop and serve a webpage that allows me to move a box with my mouse.
+//          3. Develop and serve a webpage that shows the total number of page views.
+//          4. Please develop and serve a simple todo webpage. The user should be able to add and delete todo and see all todos. Develop in a neumorphism style.
+//
+//        💬 Enter your request:
+//        """.trimIndent()
+//    )
+//
+//    val request = readln()
+//    DeveloperAgent.handleRequest(request)
+//}
