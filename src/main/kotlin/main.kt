403079fe9b02c4aa2070f6493c638c3e25513013
eeuunn <haspring@dsm.hs.kr>
2025-11-03 10:26:34 +0900

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,33 @@
 import kotlinx.coroutines.runBlocking
 import week1.presentation.WeekOneViewModel
+import week2.core.DeveloperAgent
 
-fun main() = runBlocking {
-    try {
-        val vm = WeekOneViewModel()
-        vm.callOpenAi()
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
+//fun main() = runBlocking {
+//    try {
+//        val vm = WeekOneViewModel()
+//        vm.callOpenAi()
+//    } catch (e: Exception) {
+//        e.printStackTrace()
+//    }
+//}
+
+fun main() {
+    println(
+        """
+        ============================
+          🤖 Developer Agent Started
+        ============================
+
+        📜 예시 요청:
+          1. Please develop and serve a webpage that displays hello world.
+          2. Please develop and serve a webpage that allows me to move a box with my mouse.
+          3. Develop and serve a webpage that shows the total number of page views.
+          4. Please develop and serve a simple todo webpage. The user should be able to add and delete todo and see all todos. Develop in a neumorphism style.
+          
+        💬 Enter your request:
+        """.trimIndent()
+    )
+
+    val request = readln()
+    DeveloperAgent.handleRequest(request)
 }
