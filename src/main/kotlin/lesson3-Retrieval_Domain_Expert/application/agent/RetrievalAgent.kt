73e6452d1fe13eca7254f9753df62155860171d7
eeuunn <haspring@dsm.hs.kr>
2025-11-11 09:58:35 +0900

    Update RetrievalAgent.kt

--- src/main/kotlin/lesson3-Retrieval_Domain_Expert/application/agent/RetrievalAgent.kt
+++ src/main/kotlin/lesson3-Retrieval_Domain_Expert/application/agent/RetrievalAgent.kt
@@ -1,4 +1,11 @@
 package `lesson3-Retrieval_Domain_Expert`.application.agent
 
-class RetrievalAgent {
+class RetrievalAgent(
+    private val planner: Planner,
+    private val executor: Executor
+) {
+    fun ask(question: String): String {
+        val plans = planner.plan(question)
+        return executor.execute(plans)
+    }
 }
